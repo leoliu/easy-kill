e3b2442e2096cefff94ea8656e49af07fee58f47
Leo Liu <sdl.web@gmail.com>
2014-04-22 23:36:46 +0800

    Fix copyright header for test.el

--- test.el
+++ test.el
@@ -1,3 +1,25 @@
+;;; test.el --- tests for easy-kill                  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2014  Free Software Foundation, Inc.
+
+;; Author: Leo Liu <sdl.web@gmail.com>
+;; Keywords: maint
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'easy-kill)
 (require 'ert)
 
@@ -407,3 +429,5 @@ some of the ways to customize it;
       (easy-kill-shrink)
       (should (= (mark t) (easy-kill-get start)))
       (should (= (point) (easy-kill-get end))))))
+
+;;; test.el ends here
